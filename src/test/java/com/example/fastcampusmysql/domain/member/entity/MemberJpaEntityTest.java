8044e28828e59aa6220545d195a88d0a519e4e16
jaejun1130 <wjswowns1234@gmail.com>
2023-11-05 21:07:32 +0900

    testcode 추가2

--- src/test/java/com/example/fastcampusmysql/domain/member/entity/MemberJpaEntityTest.java
+++ src/test/java/com/example/fastcampusmysql/domain/member/entity/MemberJpaEntityTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class MemberJpaEntityTest {
     @Test
-    @DisplayName("회원은 닉네임을 변경할 수 있다. (10글자 초과시 실패)")
+    @DisplayName("회원은 닉네임을 변경할 수 있다.")
     public void testChangeNickName() {
         MemberJpaEntity memberJpaEntity = MemberFixtureFactory.create();
         String expected = "test";
@@ -15,5 +15,14 @@ class MemberJpaEntityTest {
         memberJpaEntity.changeNickname(expected);
 
         Assertions.assertEquals(expected, memberJpaEntity.getNickname());
+    }
+
+    @Test
+    @DisplayName("회원은 닉네임을 변경할 수 없다. (10글자 초과시 실패)")
+    public void testNicknameMaxLength() {
+        MemberJpaEntity memberJpaEntity = MemberFixtureFactory.create();
+        String expected = "testtesttesttest";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> memberJpaEntity.changeNickname(expected));
     }
 }
